a9bf8809c8834a99a59baeb698fc195f448af4f7
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2021-10-12 00:33:38 +0200

    Removed OWID variables from ISO_CODES

--- biplots-pca.R
+++ biplots-pca.R
@@ -5,3 +5,20 @@ data <- read.csv(url("https://raw.githubusercontent.com/abarahonar/biplots-pca/m
 # window) of the daily variables.". This presents two problems:
 # 1. I don't understand what it means
 # 2. I don't think its that useful
+
+# Remove aggregations of data
+# Those are denoted as an iso_code starting with OWID
+ptn <- "^[^OWID].*" # This will match anything that doesn't start with OWID
+ndx <- grep(ptn, data$iso_code, perl = TRUE)
+data.clean <- data[ndx,]
+# To test it: unique(data.clean[c("iso_code")])
+
+# TODO: Determine which variables need to go
+# Possible variables that need to die:
+# Continent (Unnecessary)
+# Location (Redundant, can be obtained from iso_code)
+# Excess mortality information (83% of the data is NA)
+# Human development index (Unnecessary)
+# Life expectancy (Unnecessary)
+# Hand washing facilities (Unnecessary and 45% of the data is NA)
+# ICU and Hospitalization Information (At least 75% of the data is NA)
