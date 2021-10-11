5bd3129d33fc2323c3bd6b0d30ed9db5c8b8e514
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2021-10-11 21:35:44 +0200

    Initial commit
    Added main file, dataset and README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Innovative visualiztion on COVID-19 data
+Data obtained from [Our world in data's repositories](https://github.com/owid/covid-19-data/tree/master/public/data). Download date: 11/08/2021
---
+++ biplots-pca.R
@@ -0,0 +1,7 @@
+data <- read.csv("~/Projects/biplots-pca/owid-covid-data.csv", header = TRUE)
+
+# According to the source of the data, the variables that have "smoothed" in the
+# name means that "These metrics are averaged versions (over a rolling 7-day
+# window) of the daily variables.". This presents two problems:
+# 1. I don't understand what it means
+# 2. I don't think its that useful
