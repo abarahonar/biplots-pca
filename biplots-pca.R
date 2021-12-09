ae9047391cbd1a2d6bfdaf24dacf134ac947c071
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2021-12-09 09:15:22 +0100

    Pre biplot

--- biplots-pca.R
+++
@@ -1,8 +0,0 @@
-
-k <- kmeans(with.names, 5)
-
-# Basic plots for both the variables in the time series and the countries, histograms and box plots
-# Document the variables and the measurements units
-# Check multi correspondance  analysis in factomine, combine it with clustering
-# Change factoextra with factominer
-# In case the cluster plot still doesnt work, we can check the box plots for each cluster manually to see differences between them
